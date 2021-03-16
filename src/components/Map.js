69f95778880014755b32577475dee606fe3e851e
Maria Karlsson <mariahkarlsson_@hotmail.com>
2021-03-16 10:23:45 +0100

    Map one region in polygon layer

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,42 +2,10 @@ import { useState } from "react";
 import DeckGL from "@deck.gl/react";
 import { StaticMap } from "react-map-gl";
 import { polygonLine } from "./MapLayer";
+import data from "../data/regions.js";
 
 const Map = () => {
-  const mapToken = process.env.REACT_APP_MAP_TOKEN;
-
-  // initial data coords
-  const simple = [
-    [18.58511, 59.49518],
-    [18.408686, 59.010832],
-    [17.408525, 59.295714],
-    [17.732621, 59.691677],
-    [18.58511, 59.49518],
-  ];
-  // initial data coords with holes
-  const complex = [
-    [
-      [-122.4, 37.7],
-      [-122.4, 37.8],
-      [-122.5, 37.8],
-      [-122.5, 37.7],
-      [-122.4, 37.7],
-    ],
-    [
-      [-122.45, 37.73],
-      [-122.47, 37.76],
-      [-122.47, 37.71],
-      [-122.45, 37.73],
-    ],
-  ];
-
-  // coords that will change state, depending on fetch
-  const [polygonCoords, setPolygonCoords] = useState([
-    {
-      data: simple,
-    },
-  ]);
-
+  // initial viewstate
   const [viewState, setViewState] = useState({
     latitude: 62.483208,
     longitude: 16.927503,
@@ -45,21 +13,46 @@ const Map = () => {
     pitch: 15,
     bearing: 0,
   });
+  // token for mapbox api
+  const mapToken = process.env.REACT_APP_MAP_TOKEN;
+
+  const county = data[0].features[23].geometry.coordinates;
+  console.log("one county:", county);
+  console.log("data.length:", data[0].features.length);
+
+  const countyCoords = Object.entries(data[0].features).map(([x, y]) => ({
+    id: x,
+    coords: y.geometry.coordinates,
+  }));
+  console.log("map with object / coords: ", countyCoords);
+
+  const countyCode = Object.entries(data[0].features).map(([x, y]) => ({
+    id: x,
+    landskap: y.properties.landskap,
+    landskapskod: y.properties.landskapskod,
+  }));
+  console.log("map with object / codes: ", countyCode);
+
+  // coords for the polygon
+  const [polygonCoords, setPolygonCoords] = useState([
+    {
+      data: county,
+    },
+  ]);
 
   return (
     <DeckGL
       viewState={viewState}
-      layers={[polygonLine(polygonCoords)]}
       onViewStateChange={({ viewState }) => {
         setViewState({ ...viewState });
       }}
       controller={true}
+      layers={[polygonLine(polygonCoords)]}
+      getTooltip={({ object }) =>
+        object && `Landskap: ${data[0].features[23].properties.landskap}`
+      }
     >
-      <StaticMap
-        mapboxApiAccessToken={mapToken}
-        // may not be allowed due to ToS
-        // attributionControl={false}
-      />
+      <StaticMap mapboxApiAccessToken={mapToken} />
     </DeckGL>
   );
 };
