6f401eab16e0c66da1be3e34b2025f6ffa6d9d2d
Maria Karlsson <mariahkarlsson_@hotmail.com>
2021-03-16 10:38:18 +0100

    Add separate file for header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="app-header">
+      <p>Covid-19 vaccin i Sverige</p>
+    </div>
+  );
+};
+
+export default Header;
