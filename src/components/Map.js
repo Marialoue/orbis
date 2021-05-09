1b2fc788f73e2915ae3b282d050806481372156a
Maria Karlsson <mariahkarlsson_@hotmail.com>
2021-05-09 08:39:03 +0200

    Add second layer feature

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DeckGL from "@deck.gl/react";
 import { StaticMap } from "react-map-gl";
-import { geojsonLayer } from "./MapLayer";
+import { countyLayer, regionLayer, polygon } from "./MapLayer";
+
+// "mapbox://styles/mapbox/dark-v9"
 
 const Map = () => {
   // initial viewstate
   const [viewState, setViewState] = useState({
     latitude: 62.483208,
-    longitude: 16.927503,
+    longitude: 13.927503,
     zoom: 4,
     pitch: 30,
     bearing: 0,
@@ -16,16 +18,25 @@ const Map = () => {
   // token for mapbox api
   const mapToken = process.env.REACT_APP_MAP_TOKEN;
 
+  const [showCounty, setShowCounty] = useState(true);
+
+  useEffect(() => {
+    // setShowCounty(!showCounty);
+  }, []);
+
   function getTooltip({ object }) {
-    return (
-      object && {
-        html: `\
-    <div><b>County: </b>${object.properties.name}</div>
-    <div><b>Population: </b>${object.properties.population}</div>
+    return object && showCounty
+      ? {
+          html: `\
+    <div><b>County: </b>${object.properties.name}<br/>
+    <b>Population: </b>${object.properties.population}</div>
     `,
-      }
-    );
+        }
+      : null;
   }
+
+  const layers = [showCounty ? countyLayer() : regionLayer()];
+  console.log(`show county: ${showCounty}`);
 
   return (
     <DeckGL
@@ -34,11 +45,15 @@ const Map = () => {
         setViewState({ ...viewState });
       }}
       controller={true}
-      layers={[geojsonLayer()]}
-      getTooltip={getTooltip}
+      layers={layers}
+      getTooltip={({ object }) =>
+        object && showCounty
+          ? `County: ${object.properties.name}\n Population: ${object.properties.population}`
+          : null
+      }
     >
       <StaticMap
-        mapStyle={"mapbox://styles/mapbox/dark-v9"}
+        mapStyle={"mapbox://styles/mapbox/light-v8"}
         // preventStyleDiffing={true}
         // reuseMaps
         mapboxApiAccessToken={mapToken}
--- src/components/MapLayer.js
+++ src/components/MapLayer.js
@@ -1,11 +1,12 @@
 import { PolygonLayer, GeoJsonLayer } from "@deck.gl/layers";
 import counties from "../data/counties.js";
+import regions from "../data/regions.js";
 import { scaleThreshold } from "d3-scale";
 
 // export const polygon = () => {
 //   return new PolygonLayer({
 //     id: "polygon-layer",
-//     data: geoData,
+//     data: regions,
 //     pickable: true,
 //     stroked: true,
 //     filled: true,
@@ -17,24 +18,51 @@ import { scaleThreshold } from "d3-scale";
 //   });
 // };
 
+export const regionLayer = () => {
+  return new GeoJsonLayer({
+    id: "geojson-layer",
+    data: regions,
+    pickable: true,
+    stroked: true,
+    filled: true,
+    extruded: true,
+    wireframe: true,
+    lineWidthMinPixels: 1,
+    getElevation: (d) => Math.sqrt(d.properties.population),
+    getPolygon: (d) => d.data,
+    getFillColor: (d) => COLOR_SCALE(d.properties),
+    getLineColor: [80, 80, 80],
+    getLineWidth: 1,
+  });
+};
 
-const defaultMode = [
-  [247,251,255,150],
-  [222,235,247,150],
-  [198,219,239,150],
-  [158,202,225,150],
-  [107,174,214,150],
-  [66,146,198,150],
-  [33,113,181,150],
-  [8,81,156,150],
-  [8,48,107,150],
+const green = [
+  [247, 252, 185, 150],
+  [217, 240, 163, 150],
+  [173, 221, 142, 150],
+  [120, 198, 121, 150],
+  [65, 171, 93, 150],
+  [35, 132, 67, 150],
+  [0, 104, 55, 150],
+  [0, 69, 41, 150],
+];
+
+const blue = [
+  [247, 251, 255, 150],
+  [222, 235, 247, 150],
+  [198, 219, 239, 150],
+  [158, 202, 225, 150],
+  [107, 174, 214, 150],
+  [66, 146, 198, 150],
+  [33, 113, 181, 150],
+  [8, 81, 156, 150],
+  [8, 48, 107, 150],
 ];
 
 export const COLOR_SCALE = scaleThreshold()
   .domain([
     50000,
     100000,
-    150000,
     200000,
     250000,
     300000,
@@ -50,15 +78,13 @@ export const COLOR_SCALE = scaleThreshold()
     800000,
     850000,
     900000,
-    950000,
-    1100000,
-    1200000,
-    1400000,
-    1600000,
+    1000000,
+    1500000,
+    2000000,
   ])
-  .range(defaultMode);
+  .range(green);
 
-export const geojsonLayer = () => {
+export const countyLayer = () => {
   return new GeoJsonLayer({
     id: "geojson-layer",
     data: counties,
