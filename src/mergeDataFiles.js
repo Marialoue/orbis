0a6255dfeff4fa653923235544edaf79e4d94d0e
Maria Karlsson <mariahkarlsson_@hotmail.com>
2021-05-13 09:16:06 +0200

    Edit path to file

--- src/mergeDataFiles.js
+++ src/mergeDataFiles.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 let counties = fs.readFileSync("./data/counties.json");
 let countiesJson = JSON.parse(counties);
-let vaccinated = fs.readFileSync("./vaccinated.json");
+let vaccinated = fs.readFileSync("./data/vaccinated.json");
 let vaccinatedJson = JSON.parse(vaccinated);
 
 countiesJson.features.map((countyItem) => {
