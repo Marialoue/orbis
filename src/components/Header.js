256eec8d030397dcdd0e29be09213c426c7d5480
Maria Karlsson <mariahkarlsson_@hotmail.com>
2021-05-21 16:07:53 +0200

    Add mui buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,22 @@ import React from "react";
 import { ReactComponent as HelpIcon } from "../assets/help-circle.svg";
 import { ReactComponent as Settings } from "../assets/settings.svg";
 import { ReactComponent as Layers } from "../assets/layers.svg";
+import Button from "@material-ui/core/Button";
 
 const Header = () => {
   return (
     <div className="app-header">
       <h2>Covid-19 vaccin tracker</h2>
       <div className="header-menu">
-        <button onClick={console.log("help")}>
+        <Button >
           <HelpIcon />
-        </button>
-        <button onClick={console.log("layers")}>
+        </Button>
+        <Button >
           <Layers />
-        </button>
-        <button onClick={console.log("settings")}>
+        </Button>
+        <Button >
           <Settings />
-        </button>
+        </Button>
       </div>
     </div>
   );
