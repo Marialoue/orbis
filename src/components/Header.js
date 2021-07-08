e14d814ff41f361fc06235555baf4188d1fa5e02
Maria Karlsson <mariahkarlsson_@hotmail.com>
2021-07-08 14:08:29 +0200

    Add numbers for week 24 and 25

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { withRouter } from "react-router-dom";
+// import { withRouter } from "react-router-dom";
 import { ReactComponent as HelpIcon } from "../assets/help-circle.svg";
 import { ReactComponent as Layers } from "../assets/layers.svg";
 import Button from "@material-ui/core/Button";
@@ -23,8 +23,29 @@ const Header = () => {
       <div className="nav-brand">
         <a href="/">Covid-19 vaccinering | Sverige</a>
       </div>
+      <div className="nav-menu">
+        {/* <li>
+          <a href="about">
+            <Button>
+              <HelpIcon />
+            </Button>
+          </a>
+        </li>
+        <li>
+          <Button onClick={handleClick}>
+            <Layers />
+          </Button>
+          {showSub ? (
+            <select onSelect={handleSelect}>
+              <option value="option-1">option-1</option>
+              <option value="option-2">option-2</option>
+              <option value="option-3">option 3</option>
+            </select>
+          ) : null}
+        </li> */}
+      </div>
     </nav>
   );
 };
 
-export default withRouter(Header);
+export default Header;
